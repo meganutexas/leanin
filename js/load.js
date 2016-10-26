32a9937a9caa03b956ff9ccc80b8ed620d03a929
Megan Avery <megan1022@utexas.edu>
2016-10-26 18:12:20 -0500

    first commit

---
+++ js/load.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('dropdown-toggle').dropdown()
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+})
+
+$(function(){
+
+});
+
+document.registerElement('question');
+document.registerElement('answer');
+
+
